6f0d80b95461c93730c3ce311333c416ec51e99e
kailahk <kailahkorsh@me.com>
2023-05-30 10:42:21 -0700

    netflix repos log in the console

---
+++ src/components/NetflixReposList/NetflixReposList.jsx
@@ -0,0 +1,11 @@
+import "./NetflixReposList.css"
+
+export default function NetflixReposList({netflixRepos}) {
+    // const netflixRepoList = netflixRepos.map((r, idx) => {})
+    console.log(netflixRepos)
+    return (
+        <div className="NetflixReposList">
+            NetflixReposList
+        </div>
+    )
+}
--- src/pages/NetflixReposPage/NetflixReposPage.jsx
+++ src/pages/NetflixReposPage/NetflixReposPage.jsx
@@ -1,13 +1,22 @@
 import "./NetflixReposPage.css"
+import NetflixReposList from "../../components/NetflixReposList/NetflixReposList"
 import { useState } from "react"
-import {getRepos} from "../../utilities/repo-api"
+import { getRepos } from "../../utilities/repo-api"
 
 export default function NetflixReposPage() {
     const company = 'Netflix'
-    const [netflixRepos, setNetflixRepos] = useState(getRepos(company))
+    const [netflixRepos, setNetflixRepos] = useState(getResults);
+    async function getResults() {
+        try {
+            const res = await getRepos(company);
+            setNetflixRepos(res)
+        } catch (err) {
+            console.log(err)
+        }
+    }
     return (
         <div className="NetflixReposPage">
-            Netflix Repositories
+            <NetflixReposList netflixRepos={netflixRepos} />
         </div>
     )
 }
